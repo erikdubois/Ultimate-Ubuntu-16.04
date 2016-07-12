219c9593833bfa7377fe33f006d9d65b4a58f873
Erik Dubois <erik.dubois@gmail.com>
2016-07-12 14:43:37 +0200

    Commit comment : update on Tue Jul 12 14:43:37 CEST 2016

--- theme-arc-dark-install.sh
+++ theme-arc-dark-install.sh
@@ -75,11 +75,10 @@
 
 rm -rf arc-theme
 
-sudo apt-get install build-essential autoconf libgtk-3.0 libgtk-3-dev
-
+sudo apt-get install build-essential autoconf libgtk-3.0 libgtk-3-dev -y
 git clone https://github.com/horst3180/arc-theme --depth 1 && cd arc-theme
 ./autogen.sh --prefix=/usr
-sudo make install
+sudo make install -y
 
 # sudo rm -rf /usr/share/themes/{Arc,Arc-Darker,Arc-Dark}
 
